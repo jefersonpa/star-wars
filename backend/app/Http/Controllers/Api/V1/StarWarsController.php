ab1a308f3bae0bbc8ce3d63b2c3391f5aaae29a6
jefersonthimoteo <jeferson.thimoteo@mega.com.br>
2025-05-24 15:59:33 -0300

    Created the first endpoint to get all people based on the searched param

---
+++ backend/app/Http/Controllers/Api/V1/StarWarsController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use App\Http\Controllers\Controller;
+use App\Services\StarWarsService;
+
+/**
+ * @OA\Info(
+ * version="1.0.0",
+ * title="Star Wars API Documentation",
+ * description="API for accessing Star Wars data (People, Films, etc.)",
+ * @OA\Contact(
+ * email="your@email.com"
+ * ),
+ * @OA\License(
+ * name="Apache 2.0",
+ * url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ * )
+ * )
+ * @OA\Server(
+ * url="http://localhost:8000/api/v1",
+ * description="Star Wars API (Development)"
+ * )
+ */
+class StarWarsController extends Controller
+{
+    protected StarWarsService $starWarsService;
+
+    public function __construct(StarWarsService $starWarsService)
+    {
+        $this->starWarsService = $starWarsService;
+    }
+
+    /**
+     * @OA\Get(
+     * path="/star-wars/people",
+     * @OA\Parameter(
+     * name="name",
+     * in="query",
+     * required=true,
+     * description="Name of the person to search for (e.g., 'luke')",
+     * @OA\Schema(type="string")
+     * ),
+     * @OA\Response(
+     * response=200,
+     * description="Successful response",
+     * ),
+     * )
+     */
+    public function searchPeople(Request $request): JsonResponse
+    {
+        $searchTerm = $request->query('name');
+
+        if (empty($searchTerm)) {
+            return response()->json([
+                'message' => 'Please provide a "name" query parameter for the search (e.g., ?name=luke).',
+            ], 400);
+        }
+
+        $results = $this->starWarsService->searchPeople($searchTerm);
+
+        if (is_null($results)) {
+            return response()->json([
+                'message' => 'Could not retrieve people from Star Wars API.',
+            ], 500);
+        }
+
+        return response()->json([
+            'message' => 'People search successful.',
+            'data' => $results
+        ]);
+    }
+}
--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -1,11 +1,6 @@
 <?php
 namespace App\Http\Controllers;
-/**
- * @OA\Info(
- *    title="Swagger with Laravel",
- *    version="1.0.0",
- * )
- */
+
 abstract class Controller
 {
 }
