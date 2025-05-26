ca4f0e21cb18f1f2c5b29edf25d57312435936cc
jefersonthimoteo <jeferson.thimoteo@mega.com.br>
2025-05-26 18:08:36 -0300

    Added swagger to the analytics controller

--- backend/app/Http/Controllers/Api/V1/StarWarsController.php
+++ backend/app/Http/Controllers/Api/V1/StarWarsController.php
@@ -7,24 +7,6 @@ use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
 use App\Services\StarWarsService;
 
-/**
- * @OA\Info(
- * version="1.0.0",
- * title="Star Wars API Documentation",
- * description="API for accessing Star Wars data (People, Films, etc.)",
- * @OA\Contact(
- * email="your@email.com"
- * ),
- * @OA\License(
- * name="Apache 2.0",
- * url="http://www.apache.org/licenses/LICENSE-2.0.html"
- * )
- * )
- * @OA\Server(
- * url="http://localhost:8000/api/v1",
- * description="Star Wars API (Development)"
- * )
- */
 class StarWarsController extends Controller
 {
     protected StarWarsService $starWarsService;
--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -1,6 +1,21 @@
 <?php
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ * version="1.0.0",
+ * title="Star Wars API Documentation",
+ * description="API for accessing Star Wars data (People, Films, etc.)",
+ * @OA\License(
+ * name="Apache 2.0",
+ * url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ * )
+ * )
+ * @OA\Server(
+ * url="http://localhost:8000/api/v1",
+ * description="Star Wars API (Development)"
+ * )
+ */
 abstract class Controller
 {
 }
